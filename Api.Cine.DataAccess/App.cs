475fd4d9e4859f3e76a023483ef5cd7daf40fe0b
maquina 2 <maquina 2@MAQUINA2>
2022-10-06 11:38:11 -0500

    Agregar archivos de proyecto.

---
+++ Api.Cine.DataAccess/App.cs
@@ -0,0 +1,33 @@
+﻿using Api.Cine.DataAccess.DataAccess.Mock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Cine.DataAccess
+{
+    public class App
+    {
+
+
+        private static IDataAccess dataAccess;
+
+        public static IDataAccess DataAccess
+        {
+            get 
+            {
+            if(App.dataAccess==null)
+
+                {
+                    App.dataAccess = new MockDataAccess();
+
+                }
+
+                return App.dataAccess;
+            
+            }
+
+        }
+    }
+}
